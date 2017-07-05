9740234d4100db71544579d634db553184eb6091
Nishant Thapliyal <nishantt95@gmail.com>
2017-07-05 12:26:54 +0530

    CAT-2471 Adding parameters to sent Cloud Message
    This pull request includes the implementation of cloud messaging with custom parameters.

---
+++ catroid/src/main/java/org/catrobat/catroid/cloudmessaging/CloudMessaging.java
@@ -0,0 +1,101 @@
+/*
+ * Catroid: An on-device visual programming system for Android devices
+ * Copyright (C) 2010-2017 The Catrobat Team
+ * (<http://developer.catrobat.org/credits>)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * An additional term exception under section 7 of the GNU Affero
+ * General Public License, version 3, is available at
+ * http://developer.catrobat.org/license_additional_term
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.catrobat.catroid.cloudmessaging;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
+import com.google.firebase.messaging.RemoteMessage;
+
+import org.catrobat.catroid.R;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class CloudMessaging {
+
+	private static final String TAG = CloudMessaging.class.getSimpleName();
+
+	private Context context;
+	private RemoteMessage remoteMessage;
+	private Map<String, String> dataPayload;
+	private static final String IMAGE_URL = "image";
+	private static final String WEB_PAGE_URL = "link";
+
+	public CloudMessaging(Context context, RemoteMessage remoteMessage) {
+		this.context = context;
+		this.remoteMessage = remoteMessage;
+		dataPayload = remoteMessage.getData();
+	}
+
+	public Bitmap getBitmap(String link) {
+		try {
+			URL url = new URL(link);
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setDoInput(true);
+			connection.connect();
+			InputStream input = connection.getInputStream();
+			return BitmapFactory.decodeStream(input);
+		} catch (IOException e) {
+			Log.w(TAG, "Failed to download image. Using default icon", e);
+			return BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_launcher);
+		}
+	}
+
+	public String getTitle() {
+		return remoteMessage.getNotification().getTitle();
+	}
+
+	public String getMessage() {
+		return remoteMessage.getNotification().getBody();
+	}
+
+	public String getImageUrl() {
+		return dataPayload.get(IMAGE_URL);
+	}
+
+	public String getWebPageUrl() {
+		return dataPayload.get(WEB_PAGE_URL);
+	}
+
+	public boolean isValidData(ArrayList<String> notificationData) {
+
+		for (String data : notificationData) {
+			if (isStringNullOrEmpty(data)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private boolean isStringNullOrEmpty(String string) {
+		return string == null || string.isEmpty();
+	}
+}
---
+++ catroid/src/main/java/org/catrobat/catroid/cloudmessaging/CustomFirebaseMessagingService.java
@@ -0,0 +1,89 @@
+/*
+ * Catroid: An on-device visual programming system for Android devices
+ * Copyright (C) 2010-2017 The Catrobat Team
+ * (<http://developer.catrobat.org/credits>)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * An additional term exception under section 7 of the GNU Affero
+ * General Public License, version 3, is available at
+ * http://developer.catrobat.org/license_additional_term
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.catrobat.catroid.cloudmessaging;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import org.catrobat.catroid.R;
+
+import java.util.ArrayList;
+
+public class CustomFirebaseMessagingService extends FirebaseMessagingService {
+
+	public static final String TAG = CustomFirebaseMessagingService.class.getSimpleName();
+
+	@Override
+	public void onMessageReceived(RemoteMessage remoteMessage) {
+
+		CloudMessaging cloudMessaging = new CloudMessaging(this, remoteMessage);
+
+		String title = cloudMessaging.getTitle();
+		String message = cloudMessaging.getMessage();
+		String imageUrl = cloudMessaging.getImageUrl();
+		String webPageUrl = cloudMessaging.getWebPageUrl();
+
+		ArrayList<String> notificationData = new ArrayList<>();
+		notificationData.add(title);
+		notificationData.add(message);
+		notificationData.add(imageUrl);
+		notificationData.add(webPageUrl);
+
+		if (!cloudMessaging.isValidData(notificationData)) {
+			return;
+		}
+
+		Bitmap imageBitmap = cloudMessaging.getBitmap(imageUrl);
+
+		sendNotification(title, message, imageBitmap, webPageUrl);
+	}
+
+	private void sendNotification(String title, String message, Bitmap image, String link) {
+		Uri webPage = Uri.parse(link);
+		Intent intent = new Intent(Intent.ACTION_VIEW, webPage);
+		PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+		Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+		NotificationCompat.Builder notification = new NotificationCompat.Builder(this)
+				.setSmallIcon(R.drawable.ic_launcher)
+				.setLargeIcon(image)
+				.setContentTitle(title)
+				.setContentText(message)
+				.setAutoCancel(true)
+				.setSound(defaultSoundUri)
+				.setContentIntent(pendingIntent);
+
+		NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		notificationManager.notify(0, notification.build());
+	}
+}
